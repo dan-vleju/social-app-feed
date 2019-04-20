6ce7373163b2a3b4189ff1bb52109b8811a2c6dc
Dan Vleju <dan.vleju@gmail.com>
2019-04-20 19:19:12 +0300

    Initial Commit

---
+++ SocialAppFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialAppFeed
+//
+//  Created by Dan Vleju on 20/04/2019.
+//  Copyright © 2019 Dan Vleju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
